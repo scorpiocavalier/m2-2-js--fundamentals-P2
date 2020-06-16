3fc0cef5b39cc51598f49d9f6d611352ddd86bcf
Arturo Chen <art.can.fly@gmail.com>
2020-06-16 19:50:40 -0400

    E5 done

--- workshop/exercise-5.js
+++ workshop/exercise-5.js
@@ -2,7 +2,9 @@
 // Write a function that takes an array and a value as arguments and
 // returns true if the value is in the array else it returns false.
 
-function itemIsPresent(array, item) {}
+function itemIsPresent(array, item) {
+  return array.includes(item);
+}
 
 // Test case
 const myArray = [
