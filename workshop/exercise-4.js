d6dff414f99815edf463aaf9b56ac5d98835a151
Arturo Chen <art.can.fly@gmail.com>
2020-06-16 19:45:57 -0400

    E4 done

--- workshop/exercise-4.js
+++ workshop/exercise-4.js
@@ -2,7 +2,10 @@
 // Write a function that accepts 5 grades (numerical values) and returns the average.
 // The returned average should also be rounded to the nearest unit. (no decimals, please!)
 
-function calculateAverage(a, b, c, d, e) {}
+function calculateAverage(a, b, c, d, e) {
+
+    return Math.round(a+b+c+d+e)/5;
+}
 
 // make certain to console the returned value
 console.log(calculateAverage(76, 60, 83, 100, 78));
