d46aac1483693b043e5de9b17f562d91a15f9c6f
Arturo Chen <art.can.fly@gmail.com>
2020-06-16 19:40:10 -0400

    E3 done

--- workshop/exercise-3.js
+++ workshop/exercise-3.js
@@ -5,9 +5,17 @@
 // Also, wouldn't it be nice if there were a Math method that could simplify the code....
 // https://www.w3schools.com/jsreF/jsref_obj_math.asp
 
+const max = (num1, num2) => Math.max(num1, num2);
+
+
 // Q3.2
 // Without changing the function at all,
 // Can you figure out how we could use it to determine the greater number between 4 integers?
 
+max( max(num1, num2), max(num3, num4) );
+
+
 // Q3.3
 // Would this work with more integers?
+
+// Yes
