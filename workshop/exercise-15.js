8993fdcbef0ac893b8dd88908ca3e9605b3b78c0
Arturo Chen <art.can.fly@gmail.com>
2020-06-16 23:16:04 -0400

    E15 done

--- workshop/exercise-15.js
+++ workshop/exercise-15.js
@@ -4,6 +4,26 @@
 
 // Hint: consider using .filter(), .indexOf() and .lastIndexOf()
 
-function filterNonUnique(array) {}
+function filterNonUnique(array) {
+
+    // USING: .forEach(), .indexOf(), .slice()
+    // let unique = array.slice();
+    // unique.forEach((num, index) => {
+    //     let foundIndex = unique.indexOf(num, index+1);
+    //     // If it's -1 it's unique, otherwise remove it.
+    //     if(foundIndex != -1)
+    //         unique.splice(foundIndex, 1);
+    // });
+    
+    // USING: .forEach(), .indexOf, .push()
+    // let unique = [];
+    // // If it's not in the unique array, add to it.
+    // array.forEach(num => unique.indexOf(num) == -1 && unique.push(num));
+    // return unique;
+    
+    // USING: .filter(), .indexOf()
+    // Return a new array with only unique numbers.
+    return array.filter((num, index) => array.indexOf(num, index+1) == -1);
+}
 
 console.log(filterNonUnique([1, 2, 3, 3, 4, 5, 6, 7, 7, 8]));
