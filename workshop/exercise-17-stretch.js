aae9b8b019f79277ae905b9930217eef4777f536
Arturo Chen <art.can.fly@gmail.com>
2020-06-16 23:41:45 -0400

    E17-stretch done

--- workshop/exercise-17-stretch.js
+++ workshop/exercise-17-stretch.js
@@ -16,6 +16,24 @@
 // See this site for a list of "narcissistic" numbers
 // http://mathworld.wolfram.com/NarcissisticNumber.html
 
-function identifyArmstrongNumbers(num1, num2) {}
+function identifyArmstrongNumbers(num1, num2) {
+
+    let armstrongNumbers = [];
+    
+    for(let num=num1; num<=num2; num++) {
+        
+        let sum = 0;
+        let strNum = num.toString();
+        let power = strNum.length;
+    
+        for(let i=0; i<power; i++)
+            sum += Math.pow(strNum[i].valueOf(), power);
+    
+        if(sum === num)
+            armstrongNumbers.push(num);
+    }
+
+    return armstrongNumbers;
+}
 
 console.log(identifyArmstrongNumbers(100, 99999));
